b155cdd5a0b731633370c5b78b5070ea0a9b9118
santoshtiwari41 <santoshhh066@gmail.com>
2022-11-12 21:29:44 +0545

    initial commit

---
+++ chapter_04_fsModule/callBackAPI/index.js
@@ -0,0 +1,61 @@
+const fs=require('fs');
+
+// creating directory with path
+
+
+//  fs.mkdir('/home/santosh/Desktop/Nodejs/chapter_04_fsModule/callBackAPI/demo',(error)=>{
+//      if(error)throw error;
+//      console.log('directory created');
+//  })
+
+// creating directory wothout path
+
+//  fs.mkdir('/home/santosh/Desktop/Nodejs/chapter_04_fsModule/callBackAPI/demo/test',{recursive:true},(error)=>{
+//     if(error)throw error;
+//     console.log('directory created');
+// })
+
+// reading directory
+
+// fs.readdir('/home/santosh/Desktop/Nodejs/chapter_04_fsModule',(error,files)=>{
+//     if(error) throw error;
+//     for(file of files){
+//         console.log(file);
+//     }
+// })
+
+//Remove directory
+
+// fs.rmdir('/home/santosh/Desktop/Nodejs/chapter_04_fsModule/callBackAPI/demo',(error)=>{
+//     if(error) throw error;
+//     console.log('removed.....');
+// })
+
+// creating and writing in the files
+
+// fs.writeFile('readme.txt','hello programmer',(error)=>{
+//     if(error) throw error;
+//     console.log('created....');
+// })
+
+//Read file
+
+// fs.readFile('/home/santosh/Desktop/Nodejs/chapter_04_fsModule/callBackAPI/readme.txt','utf-8',(error,data)=>{
+//     if(error) throw error;
+//     console.log(data);
+// })
+
+// Append file
+
+// fs.appendFile('/home/santosh/Desktop/Nodejs/chapter_04_fsModule/callBackAPI/readme.txt',' hello santosh',(error)=>{
+//     if(error) throw error;
+//     console.log('appended....')
+// })
+
+// copy file
+
+fs.copyFile('/home/santosh/Desktop/Nodejs/chapter_04_fsModule/callBackAPI/readme.txt','copy.txt',(error)=>{
+    if(error) throw error;
+    console.log('copied....')
+})
+
--- chapter_04_fsModule/promiseBasedAPI/index.js
+++ chapter_04_fsModule/promiseBasedAPI/index.js
@@ -3,95 +3,97 @@ const { clearScreenDown } = require('readline');
 
 // creating the directory with full path
 
-// async function makeDirectory(){
-//     try{
-//         await fs.mkdir('/home/santosh/Desktop/Nodejs/chapter_04_fsModule/promiseBasedAPI/demo')
-//         console.log('directory created ....');
-//         }catch(error){
-//         console.log(error)
+async function makeDirectory(){
+    try{
+        await fs.mkdir('/home/santosh/Desktop/Nodejs/chapter_04_fsModule/promiseBasedAPI/demo')
+        console.log('directory created ....');
+        }catch(error){
+        console.log(error)
         
-//         }
-// }
-// makeDirectory();
+        }
+}
+makeDirectory();
 
 
 //creating directory without giving full path
-// async function makeDirectory2(){
-//     try{
-//         await fs.mkdir('/home/santosh/Desktop/Nodejs/chapter_04_fsModule/promiseBasedAPI/demo/demo2',{recursive:true})
-//         console.log('directory created ....');
-//         }catch(error){
-//         console.log(error)
+async function makeDirectory2(){
+    try{
+        await fs.mkdir('/home/santosh/Desktop/Nodejs/chapter_04_fsModule/promiseBasedAPI/demo/demo2',{recursive:true})
+        console.log('directory created ....');
+        }catch(error){
+        console.log(error)
         
-//         }
-// }
-// makeDirectory2()
+        }
+}
+makeDirectory2()
 
 
 // reading the directory
 
-// async function readDirectory(){
+async function readDirectory(){
      
-// try
-// {
-//     const files =await fs.readdir('/home/santosh/Desktop/Nodejs/chapter_04_fsModule');
-//     for(const file of files)
-//     {
-//         console.log(file);
-//     }
-// }catch(error){
-//     console.log(error);
-// }
+try
+{
+    const files =await fs.readdir('/home/santosh/Desktop/Nodejs/chapter_04_fsModule');
+    for(const file of files)
+    {
+        console.log(file);
+    }
+}catch(error){
+    console.log(error);
+}
 
-// }
-// readDirectory();
+}
+readDirectory();
+
+//Remove directory
 
 
-    // async function removeDirectory(){
-    //     try{
-    //         await fs.rmdir('/home/santosh/Desktop/Nodejs/chapter_04_fsModule/promiseBasedAPI/demo')
-    //     console.log('directory removed.....');
-    //     }catch(error){
-    //         console.log(error);
-    //     }
-    // }
-    // removeDirectory();
+    async function removeDirectory(){
+        try{
+            await fs.rmdir('/home/santosh/Desktop/Nodejs/chapter_04_fsModule/promiseBasedAPI/demo')
+        console.log('directory removed.....');
+        }catch(error){
+            console.log(error);
+        }
+    }
+    removeDirectory();
 
     //create and write files
 
-    // async function wirteFile(){
-    //     try{
-    //         await fs.writeFile('readme.txt','hello programmer');
-    //         console.log('created....')
-    //     }catch(error){
-    //         console.log(error);
-    //     }
-    // }
-    // wirteFile();
+    async function wirteFile(){
+        try{
+            await fs.writeFile('readme.txt','hello programmer');
+            console.log('created....')
+        }catch(error){
+            console.log(error);
+        }
+    }
+    wirteFile();
 
     // Read file
 
-    // async function readFile(){
-    //     try{
-    //         let a=await fs.readFile('readme.txt','utf-8');
-    //         console.log(a);
-    //     }catch(error){
-    //         console.log(error);
-    //     }
-    // }
-    // readFile();
+    async function readFile(){
+        try{
+            let a=await fs.readFile('readme.txt','utf-8');
+            console.log(a);
+        }catch(error){
+            console.log(error);
+        }
+    }
+    readFile();
 
 // Append the file
 
-// async function appendFile(){
-//     try{
-//         await fs.appendFile('readme.txt','hello santosh')
-//         console.log('appended');
-//     }catch(error){
-//         console.log(error);
-//     }
-// }
-// appendFile();
+async function appendFile(){
+    try{
+        await fs.appendFile('readme.txt','hello santosh')
+        console.log('appended');
+    }catch(error){
+        console.log(error);
+    }
+}
+appendFile();
 
 // To copy the file
 
